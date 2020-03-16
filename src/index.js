c99e32b13f14804aa52dee7d09aaebd992358f0c
yegao <familyink@163.com>
2020-03-17 00:09:35 +0800

    略微有些思路

--- src/index.js
+++ src/index.js
@@ -19,25 +19,7 @@ const insertTags = (chunks, execList, before, after) => {
 
 const languageMap = {
     "JavaScript": {
-        marks: ["js", "javascript"],
-        rules: [
-            {
-                type: "backquote", // 反引号字符串 优先级最高， 因为即使注释在里面都会被忽略
-                regexp: /\`[\s\S\n\r]*?\`/g
-            },
-            {
-                type: "comment", // 注释
-                regexp: /\/\*[\s\S\n\r]*?\*\/|\/\/.*/g
-            },
-            // {
-            //     type: "dot",
-            //     regexp: /\./g
-            // },
-            // {
-            //     type: "var",
-            //     regexp: /(var|const|let)\s/g
-            // }
-        ]
+        marks: ["js", "javascript"]
     }
 }
 
@@ -66,44 +48,82 @@ const runRules = (source, rules, prefix="book-hl-") => {
     return source;
 }
 
-const hl = (element) => {
+// x`y`z
+//  * 第一种情况：x和z中不存在"、'
+//  * 第二种情况：x存在"、'
+//  * 第三种情况：x存在/*
+//  * 第四种情况：x存在//（同一行）
+//  * 第五种情况：
+//  * 第六种情况：
+// 最先找到的是`
+const list = [
+    {type:"backquote", chunk:"xxx"},
+    {}
+]
+
+const mountSource = source => {
+    // 找出source中的所有 ` " ' /* */ // / /
+    const list = [];
+    // 对于正则的判断可能需要用到断言
+    const regexp = /\`[\s\S]*?[\`$]|\".*?[\"$]|\'.*?[\'$]|\/\*[\s\S]*?(\*\/|$)|\/\/.*|\/+?[^\\]\/ *[^\_\$a-zA-Z0-9]|[^\`\"\'\/$]*?/g;
+    // 关键字 [\s\^]var\s|[\s\^]const[\s$]|[\s\^]let\s[\s$]
+    // 对象 \{[\s\S]*?\}
+    // 箭头函数 [\s\^]\=\>\s[\s$\{\}]|[\s\^]let\s[\s$]|[\s\^]let\s[\s$]
+    // 函数表达式 [\s\^]\=\>\s[\s$\{\}]|[\s\^]let\s[\s$]|[\s\^]let\s[\s$]
+    // 函数声明·· [\s\^]function\s*\(.*\)[\s\n]*\{[\s\S]*?\}
+    while(data = regexp.exec(source)) {
+        list.push({chunk: data[0], index: data.index});
+    }
+    // 得出这样的一个数组 [{type: "`", index: 8}, {type: "/*", index: 16}, {type: "*/", index: 18}, {type: """, index: 27}, {type: "'", index: 34}, {type: "//", index: 41}]
+    return list;
+}
+
+
+const hl = element => {
     // 判断语言
     let mark = element.className.toLowerCase().replace("language-","");
     const language = getLanguage(mark);
     // 获取源码
     const source = element.textContent;
     // 递归整个源码
-    const lines = source.split("\n");
-    const len = lines.length;
-    const map = {
-        backquote:[],
-        comment:[],
-        beginComment:[],
-        endComment:[]
-    }
-    for(let i = 0; i< len; i++) {
-        const line = lines[i];
-        let backQuoteIndex = 0, commentIndex = 0, beginCommentIndex = 0, endCommentIndex = 0;
-        if(backQuoteIndex = ~line.indexOf("`")) {
-            map.backquote.push([i, ~backQuoteIndex]);
-        };
-        if(commentIndex = ~line.indexOf("\/\/")) {
-            map.comment.push([i, ~commentIndex])
-        };
-        if(beginCommentIndex = ~line.indexOf("\/\*")) {
-            map.beginComment.push([i, ~beginCommentIndex])
-        };
-        if(endCommentIndex = ~line.indexOf("\*\/")) {
-            map.endComment.push([i, ~endCommentIndex])
-        };
-    }
-    console.log(map);
-    
+    const list = mountSource(source);
     // 获取规则
-    // const rules = languageMap[language].rules;
-    
+    for(const item of list) {
+        const chunk = item.chunk;
+        const index = item.index
+        // 反引号
+        if(/^\`/.test(chunk)) {
+            list.push({type: 0, chunk, index});
+        }
+        // 双引号
+        else if(/^\"/.test(chunk)) {
+            list.push({type: 1, chunk, index});
+        }
+        // 单引号 
+        else if(/^\'/.test(chunk)) {
+            list.push({type: 2, chunk, index});
+        }
+        // 多行注释
+        else if(/^\/\*/.test(chunk)) {
+            list.push({type: 3, chunk, index});
+        }
+        // 单行注释
+        else if(/^\/\//.test(chunk)) {
+            list.push({type: 4, chunk, index});
+        }
+        // 正则表达式
+        else if(/^\//.test(chunk)) {
+            list.push({type: 5, chunk, index});
+        }
+        // 正常代码
+        else {
+            list.push({type: 6, chunk, index});
+        }
+    }
+    console.log(list);
     // 运行规则
     // element.innerHTML = runRules(source, rules);
+
 }
 
 export default hl;
